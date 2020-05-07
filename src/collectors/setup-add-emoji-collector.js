85625e457748606f9e6125cacd92ee00455f2f2a
Jos Vennik <josvennik@gmail.com>
2020-05-07 17:46:44 +0900

    Reworked collector for removal of role

--- src/collectors/setup-add-emoji-collector.js
+++ src/collectors/setup-add-emoji-collector.js
@@ -37,6 +37,31 @@ export const setupAddEmojiCollector = async function setupAddEmojiCollector(
       }
     });
   });
+
+  collector.on("remove", reaction => {
+    if (!user.bot) {
+      const member = msg.guild.member(user.id);
+      if (member !== null) {
+        const grantRole = msg.guild.roles.find(
+          role => role.id === settings.initial_role
+        );
+
+        const removeRole = msg.guild.roles.find(
+          role => role.id === settings.grant_role
+        );
+
+        member.addRole(grantRole).catch(err => {
+          console.error("Failed to grant initial_role");
+          console.error(err);
+        });
+
+        member.removeRole(removeRole).catch(err => {
+          console.error("Failed to remove membership");
+          console.error(err);
+        });
+      }
+    }
+  });
 };
 
 export default setupAddEmojiCollector;
