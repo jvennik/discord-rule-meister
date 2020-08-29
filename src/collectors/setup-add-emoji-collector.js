f2eaba63ad88369058c9a5559e260e285d223071
Jos Vennik <josvennik@gmail.com>
2020-08-30 08:21:29 +0900

    Rebinding the emojiCollector periodically now

--- src/collectors/setup-add-emoji-collector.js
+++ src/collectors/setup-add-emoji-collector.js
@@ -11,7 +11,11 @@ export const setupAddEmojiCollector = async function setupAddEmojiCollector(
   const settings = await dbget(sql, [msg.guild.id]);
 
   const filter = (reaction, user) => reaction.emoji.name === "✅";
-  const collector = msg.createReactionCollector(filter);
+  const collector = msg.createReactionCollector(filter, {
+    time: 1000 * 60 * 30,
+    dispose: true
+  });
+
   collector.on("collect", reaction => {
     reaction.users.forEach(user => {
       if (!user.bot) {
@@ -38,33 +42,9 @@ export const setupAddEmojiCollector = async function setupAddEmojiCollector(
     });
   });
 
-  // NOT FUNCTIONAL
-  // collector.on("remove", reaction => {
-  //   reaction.users.forEach(user => {
-  //     if (!user.bot) {
-  //       const member = msg.guild.member(user.id);
-  //       if (member !== null) {
-  //         const grantRole = msg.guild.roles.find(
-  //           role => role.id === settings.initial_role
-  //         );
-
-  //         const removeRole = msg.guild.roles.find(
-  //           role => role.id === settings.grant_role
-  //         );
-
-  //         member.addRole(grantRole).catch(err => {
-  //           console.error("Failed to grant initial_role");
-  //           console.error(err);
-  //         });
-
-  //         member.removeRole(removeRole).catch(err => {
-  //           console.error("Failed to remove membership");
-  //           console.error(err);
-  //         });
-  //       }
-  //     }
-  //   });
-  // });
+  collector.on("end", async () => {
+    await setupAddEmojiCollector(msg);
+  });
 };
 
 export default setupAddEmojiCollector;
